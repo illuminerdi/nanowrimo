36388e728c7390bc7684ad23954e96d84459d47e
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-05-25 21:28:54 -0700

    version bump with new functionality.

--- lib/nanowrimo.rb
+++ lib/nanowrimo.rb
@@ -10,7 +10,7 @@ require 'nanowrimo/region'
 require 'nanowrimo/genre'
 
 module Nanowrimo
-  VERSION = '0.5'
+  VERSION = '0.6'
   API_URI = 'http://www.nanowrimo.org/wordcount_api'
   GOAL = 50_000
 
